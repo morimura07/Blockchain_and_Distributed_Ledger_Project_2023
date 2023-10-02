13a89af2e2247d8e69555d19b85787ca9c873d57
morimura07 <morimuradin@gmail.com>
2023-10-02 17:03:05 +0200

    added contracts in the flutter project

---
+++ flutter/gym_app/contracts/BossNFT.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; // NFT's standard
+import "@openzeppelin/contracts/access/Ownable.sol"; // to handle the ownership of the contract
+import "@openzeppelin/contracts/utils/Counters.sol"; // to auto-increment the tokenID
+
+/**
+Contract for the Boss of the Gym, only one Owner can mint these NFTs.
+*/
+contract BossNFT is ERC721, Ownable {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+    
+    constructor() ERC721("BossNFT", "BSS") {}
+
+    /// The tokenId is auto-incremented
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    // Instead of totalSupply we just ask which is the current tokenIdCounter
+    function lastTokenId() external view returns (uint256) {
+        return _tokenIdCounter.current();
+    }
+
+}

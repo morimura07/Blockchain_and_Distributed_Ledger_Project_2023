c3556dcfc7c68e92cdfc2471a10cb6aa4eb3422b
morimura07 <morimuradin@gmail.com>
2023-10-04 16:26:58 +0200

    wrote the deployment script

--- flutter/smart-contracts/migrations/2_deploy_contracts.js
+++ flutter/smart-contracts/migrations/2_deploy_contracts.js
@@ -1,5 +1,14 @@
-var SmartContract=artifacts.require ("BossNFT.sol");
+var bossNFT = artifacts.require("BossNFT.sol");
+var adminNFT = artifacts.require("AdminNFT.sol");
+var customerNFT = artifacts.require("CustomerNFT.sol");
 
-module.exports = function(deployer) {
-    deployer.deploy(SmartContract);
+module.exports = async function(deployer) {
+    // deployer.deploy(bossNFT).then(
+    //     function() {
+    //         return deployer.deploy(adminNFT, bossNFT.address);
+    //     }
+    // );
+    await deployer.deploy(bossNFT);
+    await deployer.deploy(adminNFT, bossNFT.address);
+    await deployer.deploy(customerNFT, adminNFT.address);
 }

33b46a8d9570067c42c4ccdd71ac25ddaf83e732
morimura07 <morimuradin@gmail.com>
2023-10-04 12:46:44 +0200

    Chagne of the solidity version, because the depoyment on Ganache was returning opcodes invalid.

--- flutter/smart-contracts/contracts/BossNFT.sol
+++ flutter/smart-contracts/contracts/BossNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity >=0.4.22 <0.9.0; 
 
 
 import "@openzeppelin/contracts/access/Ownable.sol"; // to handle the ownership of the contract

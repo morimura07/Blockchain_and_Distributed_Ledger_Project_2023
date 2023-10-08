3039ec6ac4b93a4fd56acc6cf4e758c36f10e956
morimura07 <morimuradin@gmail.com>
2023-10-08 22:51:51 +0200

    renamed and refactor

---
+++ flutter/gym_app/lib/view_model_smart_contracts/boss_nft_contract_vm.dart
@@ -0,0 +1,12 @@
+import 'package:gym_app/view_model_smart_contracts/nft_smart_contract.dart';
+
+
+/// It represent the ViewModel of BossNFT contract. \
+/// It's implemented as a ChangeNotifier, it means we can use it as a **state** for
+/// our application.
+class BossNFTcontractVM extends SmartContractNFT {
+
+  BossNFTcontractVM() : super("BossNFT");
+
+  
+}
